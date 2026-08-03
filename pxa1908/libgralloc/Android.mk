cd29367f2088274c97923482b02dc416546284d5
notnoelchannel2 <notnoelchannel1@protonmail.com>
2026-08-03 18:04:28 +0200

    Update Android.mk

--- pxa1908/libgralloc/Android.mk
+++ pxa1908/libgralloc/Android.mk
@@ -49,7 +49,7 @@ LOCAL_SRC_FILES := \
 LOCAL_PRELINK_MODULE := false
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-LOCAL_SHARED_LIBRARIES := $(common_libs) libbinder libmvmem libGAL
+LOCAL_SHARED_LIBRARIES := $(common_libs) libbinder libmvmem libGAL liblog
 LOCAL_C_INCLUDES       := $(common_includes) $(kernel_includes)
 
 # See hardware/libhardware/modules/README.android to see how this is named.
